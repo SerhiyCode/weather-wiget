41d2ca1e7b96abb2ee2209b65b628b67d3f3218f
Serhiy <seregarachuk20@gmail.com>
2025-10-13 16:57:36 +0300

    weather wiget

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import "./index.css";
+
+const KEY = "f005cdafaa254c57b1f120118251310";
+
+
+function App() { 
+  const [city, setCity] = useState("");  
+  const [watherData, setWatherData] = useState(null); 
+  
+
+  useEffect(() => {async function getData() {
+     const res = await fetch("http://api.weatherapi.com/v1/current.json?key=f005cdafaa254c57b1f120118251310&q=Ukraine");
+     
+     const data = await res.json(); 
+     setWatherData(data);
+  } 
+    getData() 
+}, []);
+ 
+console.log(watherData);
+
+
+  return ( 
+    <div className="app">
+      <div className="widget-container"> 
+        <button></button>
+        <div className="weather-card-container">
+          <h1 className="app-title">Weather Widget</h1>
+          <div className="search-container">
+            <input type="text" placeholder="Enter city name" className="search-input" />
+          </div>
+        </div>
+        <div className="weather-card">
+          <h2>Kiev Ukraine</h2>
+          <img src="" alt="icon" className="weather-icon" />
+          <p className="temperature">11°C</p>
+          <p className="condition">rainy</p>
+          <div className="weather-details">
+            <p>Humidity: 20%</p>
+            <p>Wind: 22 km/h</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
