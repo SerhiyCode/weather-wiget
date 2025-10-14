e397ad91d7f555a4ae1fae0b08d882451eb425d1
Serhiy <seregarachuk20@gmail.com>
2025-10-14 16:55:46 +0300

    add temp condition weather icon

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}`);      
 
      const data = await res.json();
-     console.log(data);    
+     
       if(data.error) {
          setError(data.error.message); 
         }
@@ -29,25 +29,26 @@ function App() {
     getData(); 
 }, [city]);
       
+console.log(weatherData);
 
 
   return ( 
     <div className="app">
       <div className="widget-container">          
         <div className="weather-card-container">
-          <h1 className="app-title"> W eather Widget </h1>
+          <h1 className="app-title"> Weather  Widget </h1>
           <div className="search-container">  
             <input type="text" placeholder="Enter city name" className="search-input" />
           </div>
         </div> 
       <div className="weather-card">
           <h2>{`${weatherData?.location?.name}, ${weatherData?.location?.country}`}</h2>
-          <img src="" alt="icon" className="weather-icon" />
-          <p className="temperature">11°C</p>
-          <p className="condition">rainy</p>
+          <img src={`https:${weatherData?.current?.condition?.icon}`} alt="icon" className="weather-icon" />
+          <p className="temperature"> {Math.round(weatherData?.current?.temp_c)}.C</p> 
+          <p className="condition">{weatherData?.current?.condition?.text}</p>
           <div className="weather-details">
-            <p>Humidity: 20%</p>
-            <p>Wind: 22 km/h</p>
+            <p>Humidity:{weatherData?.current?.humidity}%</p>
+            <p>{weatherData?.current?.wind_kph}km/h</p>
           </div>
         </div>        
       </div> 
