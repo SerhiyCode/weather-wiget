25dfb1d0f8f9c343cfcd24f78376895b9c1a30f7
Serhiy <seregarachuk20@gmail.com>
2025-10-15 15:44:22 +0300

    add function  render loading error

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,60 @@ import "./index.css";
 const KEY = "f005cdafaa254c57b1f120118251310";
 
 function App() { 
-  const [city, setCity] = useState("Ukraine");  
+  const [city, setCity] = useState("");  
   const [weatherData, setWeatherData] = useState(null); 
-  const [error, setError] = useState(null);
+  const [error, setError] = useState(null); 
+  const [loading, setLoading] = useState(false);
 
-     useEffect(() => {  
-    getData
-    async function getData() { 
-      try{
-        const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}`);      
+  useEffect(() => {  
+  async function getData() { 
+    if (!city) return;
 
-     const data = await res.json();
+    setLoading(true); 
+    try {
+      const res = await fetch(`https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}`);      
+      const data = await res.json();
      
-      if(data.error) {
-         setError(data.error.message); 
-        }
-
-     setWeatherData(data);
-     } catch(err) {        
-        setError(err.message);
-        setWeatherData()
-     }
+      if (data.error) {
+         setError(data.error.message);  
+         setWeatherData(null); 
+         return;    
+        } 
+        setWeatherData(data); 
+        setError(null);
+    } catch { 
+       setError("Failed to fetch weather data");
+       setWeatherData(null); 
+    } finally {
+      setLoading(false);
+    }  
   } 
-    getData(); 
-}, [city]);
-      
-console.log(weatherData);
 
+  getData(); 
+}, [city]);
+    
+function renderError() {
+  return <p>{error}</p>
+}
+
+function renderLoading() {
+   return <p>Loading...</p>
+}
+
+function readerWeather() {
+  return (
+       <div className="weather-card">
+            <h2>{`${weatherData?.location?.name}, ${weatherData?.location?.country}`}</h2>
+          <img src={`https:${weatherData?.current?.condition?.icon}`} alt="icon" className="weather-icon" />
+          <p className="temperature"> {Math.round(weatherData?.current?.temp_c)}.C</p> 
+          <p className="condition">{weatherData?.current?.condition?.text}</p>
+          <div className="weather-details">
+            <p>Humidity:{weatherData?.current?.humidity}%</p>
+            <p>wind: {weatherData?.current?.wind_kph}km/h</p>
+          </div> 
+        </div>
+     );            
+}
 
   return ( 
     <div className="app">
@@ -45,16 +72,9 @@ console.log(weatherData);
              onChange={(e)=> setCity(e.target.value)} />
           </div>
         </div> 
-      <div className="weather-card">
-          <h2>{`${weatherData?.location?.name}, ${weatherData?.location?.country}`}</h2>
-          <img src={`https:${weatherData?.current?.condition?.icon}`} alt="icon" className="weather-icon" />
-          <p className="temperature"> {Math.round(weatherData?.current?.temp_c)}.C</p> 
-          <p className="condition">{weatherData?.current?.condition?.text}</p>
-          <div className="weather-details">
-            <p>Humidity:{weatherData?.current?.humidity}%</p>
-            <p>{weatherData?.current?.wind_kph}km/h</p>
-          </div>
-        </div>        
+           {error &&  renderError()} 
+           {loading &&  renderLoading()}  
+           {!loading && !error && weatherData && readerWeather()}    
       </div> 
     </div>
   );
