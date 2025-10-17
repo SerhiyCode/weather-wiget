551ba64e81d7184f87fe21aa8a48156b49b71c3d
Serhiy <seregarachuk20@gmail.com>
2025-10-17 12:37:40 +0300

    with side effects using event handlers

---
+++ src/App-v2.jsx
@@ -0,0 +1,152 @@
+import { useState, useEffect } from "react";
+import "./index.css";
+
+const KEY = "f005cdafaa254c57b1f120118251310";
+
+function App() { 
+  const [city, setCity] = useState("");  
+  const [weatherData, setWeatherData] = useState(null); 
+  const [error, setError] = useState(null); 
+  const [loading, setLoading] = useState(false); 
+  const [coords, setCoords] = useState(null); 
+
+
+  useEffect(()=> { 
+
+    if(!navigator.geolocation){
+      setError("Geolocation is not supported  by your browser");
+      return 
+    } 
+    navigator.geolocation.getCurrentPosition((position)=> { 
+      console.log(position);
+      const  {lat, lon} = position.coords; 
+      setCoords({lat, lon});            
+    },
+      (err) => {
+          console.log("Geolocation error", err.message ); 
+          setError("Could not get your location" )
+      }  
+    );
+  }, [])
+
+    
+  async function handleCityChange(e) {
+
+    const newCity = e.target.value
+    setCity(newCity); 
+
+    
+    if(!city.trim() && !coords ) {
+      setWeatherData(null);
+      setError(null); 
+      return  
+    }
+
+
+
+    setLoading(true); 
+    try {
+
+
+      const query = newCity.trim() ? city : `${coords.tat}, ${coords.lon}`
+      const res = await fetch(`https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${query}`);      
+      const data = await res.json();
+     
+      if (data.error) {
+         setError(data.error.message);  
+         setWeatherData(null); 
+         return;    
+        } 
+        setWeatherData(data); 
+        setError(null);
+    } catch { 
+       setError("Failed to fetch weather data");
+       setWeatherData(null); 
+    } finally {
+      setLoading(false);
+    }  
+  } 
+
+ 
+
+
+//   async function getData() { 
+//     if (!city) return;
+
+//     setLoading(true); 
+//     try {
+
+//       const query = city.trim() ? city : `${coords.tat}, ${coords.lon}`
+//       const res = await fetch(`https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${query}`);      
+//       const data = await res.json();
+     
+//       if (data.error) {
+//          setError(data.error.message);  
+//          setWeatherData(null); 
+//          return;    
+//         } 
+//         setWeatherData(data); 
+//         setError(null);
+//     } catch { 
+//        setError("Failed to fetch weather data");
+//        setWeatherData(null); 
+//     } finally {
+//       setLoading(false);
+//     }  
+//   } 
+//   getData();  
+//   return ()=>{
+//     controller.abort()
+//   }
+
+// }, [city, coords]);
+    
+function renderError() {
+  return <p>{error}</p>
+}
+
+function renderLoading() {
+   return <p>Loading...</p>
+}
+
+function readerWeather() {
+  return (
+       <div className="weather-card">
+            <h2>{`${weatherData?.location?.name}, ${weatherData?.location?.country}`}</h2>
+          <img src={`https:${weatherData?.current?.condition?.icon}`} alt="icon" className="weather-icon" />
+          <p className="temperature"> {Math.round(weatherData?.current?.temp_c)}.C</p> 
+          <p className="condition">{weatherData?.current?.condition?.text}</p>
+          <div className="weather-details">
+            <p>Humidity:{weatherData?.current?.humidity}%</p>
+            <p>wind: {weatherData?.current?.wind_kph}km/h</p>
+          </div> 
+        </div>
+     );            
+}
+
+  return ( 
+    <div className="app"> 
+      <div className="widget-container">          
+        <div className="weather-card-container">
+          <h1 className="app-title"> Weather  Widget </h1>
+          <div className="search-container">  
+            <input type="text" 
+             value={city}
+             placeholder="Enter city name"
+             className="search-input"
+             onChange={ handleCityChange} />
+          </div>
+        </div> 
+           {error &&  renderError()} 
+           {loading &&  renderLoading()}  
+           {!loading && !error && weatherData && readerWeather()}    
+      </div> 
+    </div>
+  );
+}
+
+
+
+
+
+export default App;
